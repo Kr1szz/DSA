b08434e81e80b55e9a82c6f4246848be45136bed
kr1szz <kriszzgfx@gmail.com>
2026-02-04 22:57:46 +0530

    lc 121.1

---
+++ GfG and LeetCode/cpp/Basics/besttimetosell.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+
+        int min = INT_MAX;
+        int max = 0;
+        for (int price : prices)
+        {
+            if (price < min)
+                min = price;
+            else if (price - min > max)
+                max = price - min;
+        }
+        return max;
+    }
+};
